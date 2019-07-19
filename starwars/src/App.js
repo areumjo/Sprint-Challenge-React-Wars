2e246fe2732935d497dfde1b86a6932c21208695
Areum Jo <areumjo1@gmail.com>
2019-07-19 11:30:49 -0400

    Fetched SW people data and saved it to state

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+
+import axios from "axios";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -9,6 +11,19 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const [characters, setCharacters] = useState([]);
+
+  useEffect( () => {
+    axios
+      .get(`https://swapi.co/api/people/`)
+      .then(response => {
+        console.log(response.data.results);
+        const characters = response.data.results;
+        setCharacters(characters);
+      })
+      .catch(err => console.log('SWAPI is down: ', err))
+  }, []);
+  console.log(characters[1]);
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
